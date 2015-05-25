64cf22070134073110306dbdd8a2638f7ac1d1af
Adrian <leftshiftlp@gmail.com>
2015-05-25 21:17:53 +0200

    Turning off if space is closed

--- traffic-mvg.py
+++ traffic-mvg.py
@@ -2,6 +2,8 @@ import paho.mqtt.client as mqtt
 import MVGLive
 import time
 import config
+import requests
+url = "http://status.munichmakerlab.de/api.php"
 
 topic_red = "mumalab/room/trafficlight/red"
 topic_yellow = "mumalab/room/trafficlight/yellow"
@@ -16,23 +18,36 @@ def on_disconnect(client, userdata, rc):
 def on_log(client, userdata, level, buf):
 	print(buf)
 
+prev = 0
 
 mqttc = mqtt.Client("")
 mqttc.username_pw_set(config.username, config.password)
-mqttc.connect(config.host, config.port, 60)
+# mqttc.connect(config.host, config.port, 60)
 mqttc.on_connect = on_connect
 mqttc.on_disconnect = on_disconnect
 mqttc.on_log = on_log
 
 foo = MVGLive.MVGLive()
 
+state_1 = True
+state_2 = True
+state_3 = True
+
+
 def set_single_light(light, state):
-	if light == 1:
+	global state_1
+	global state_2
+	global state_3
+
+	if light == 1 and state_1 != state:
 		mqttc.publish(topic_red, int(state), 1, False)
-	elif light == 2:
+		state_1 = state
+	elif light == 2 and state_2 != state:
 		mqttc.publish(topic_yellow, int(state), 1, False)
-	elif light == 3:
+		state_2 = state
+	elif light == 3 and state_3 != state:
 		mqttc.publish(topic_green, int(state), 1, False)
+		state_3 = state
 
 def set_traffic_light(light):
 	if light == 0:
@@ -52,8 +67,18 @@ def set_traffic_light(light):
 		set_single_light(2, False)
 		set_single_light(3, True)
 
+def refresh():
+	r = requests.get(url)
+	j =  r.json()
+	if j[u'door'] == "open":
+		push_u()
+	else:
+		set_traffic_light(0)
 
-def refresh(): # Pushes the Lines to the Display
+
+def push_u(): # Pushes the Lines to the Display
+	# TODO: look at http://status.munichmakerlab.de/simple.php
+
 	lifedata = None
 	lifedata = foo.getlivedata("Obersendling")
 	reduced_lifedata = []
@@ -69,21 +94,30 @@ def refresh(): # Pushes the Lines to the Display
 	if dept["time"] < 7 and dept["time"] > 5:
 		print "green"
 		set_traffic_light(3)
+		return 3
 
 	elif dept["time"] <= 5 and dept["time"] >= 4:
 		print "yellow"
 		set_traffic_light(2)
+		return 2
 
 	else:
 		print "red"
 		set_traffic_light(1)
+		return 1
 
 # Loop
 
+mqttc.connect(config.host, config.port, 60)
+
 try:
 		print("Entered loop")
+		set_single_light(1, False)
+		set_single_light(2, False)
+		set_single_light(3, False)
 		while True:
-				refresh()
+				prev = refresh()
 				time.sleep(10)
 except KeyboardInterrupt:
 		print 'interrupted!'
+		mqttc.disconnect()
