a9fb73dbaa906fb9c86670881e25fd34a8bffde4
Adrian <leftshiftlp@gmail.com>
2015-09-11 16:24:57 +0200

    Removed unnecessary connects and disconnects

--- traffic-mvg.py
+++ traffic-mvg.py
@@ -49,21 +49,38 @@ def set_single_light(light, state):
 def set_traffic_light(light):
 	global state
 	if light == 0 and state != "000":
+		connect_once()
 		mqttc.publish(config.topic_ryg, "000", 1, False)
 		state = "000"
+		mqttc.disconnect()
 	elif light == 1 and state != "100":
+		connect_once()
 		mqttc.publish(config.topic_ryg, "100", 1, False)
 		state = "100"
+		mqttc.disconnect()
 	elif light == 2 and state != "010":
+		connect_once()
 		mqttc.publish(config.topic_ryg, "010", 1, False)
 		state = "010"
+		mqttc.disconnect()
 	elif light == 3 and state != "001":
+		connect_once()
 		mqttc.publish(config.topic_ryg, "001", 1, False)
 		state = "001"
+		mqttc.disconnect()
 	elif light == 4 and state != "110":
+		connect_once()
 		mqttc.publish(config.topic_ryg, "110", 1, False)
 		state = "110"
+		mqttc.disconnect()
 
+def connect_once():
+	try:
+		mqttc.connect(config.host, config.port, 60)
+	except Exception as e:
+		print("Failed to connect to mqtt. Retrying...")
+		time.sleep(5)
+		return False
 
 ## Values for U-Bahn
 station_u = "Obersendling"
@@ -122,12 +139,6 @@ def push(station, linename, destination, walking_time): # Pushes the Lines to th
 		return
 	dept = reduced_lifedata[0]
 
-	try:
-		mqttc.connect(config.host, config.port, 60)
-	except Exception as e:
-		print("Failed to connect to mqtt. Retrying...")
-		time.sleep(5)
-		return False
 
 	print dept["time"]
 
